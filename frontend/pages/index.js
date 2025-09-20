ace1ae0827c08de6fac1fa062afdb2e1c64d08ec
acidlord <acidlord_34@abv.bg>
2025-09-20 23:56:05 +0300

    _app.js added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,6 @@
 import { useEffect, useState } from 'react';
 import { fetchRecipes, deleteRecipe } from '../utils/api';
 import RecipeForm from '../components/RecipeForm';
-import '../styles.css';
 
 export default function Home() {
   const [recipes, setRecipes] = useState([]);
